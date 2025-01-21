1585338750f83e58721eb01540313228fa52574f
samyuktha215 <samyuktha.basam@gmail.com>
2025-01-21 14:51:32 +0100

    Added anEmptyShoppingCart method in AddingProductsSteps

--- src/test/java/org/example/stepdefs/AddingProductsSteps.java
+++ src/test/java/org/example/stepdefs/AddingProductsSteps.java
@@ -1,5 +1,17 @@
 package org.example.stepdefs;
 
+import io.cucumber.java.en.Given;
+import java.util.HashMap;
+
+
+
 public class AddingProductsSteps {
-    private  final CartManager cartManager=new CartManager();
+    private  HashMap<Integer,String>shoppingCart;
+    private int totalPrice;
+
+    @Given("an empty shopping cart ")
+    public void anEmptyShoppingCart() {
+        shoppingCart = new HashMap<>();
+        totalPrice= 0;
+    }
 }
