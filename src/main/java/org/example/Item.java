9623e1216c61a7c423aa9bc6095216784ef124bd
samyuktha215 <samyuktha.basam@gmail.com>
2025-01-23 16:42:51 +0100

    Added Item class to clear cucumberTest

---
+++ src/main/java/org/example/Item.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class Item {
+    private String itemName;
+    private int Quantity;
+    private int price;
+
+    public Item(String itemName, int quantity, int price) {
+        this.itemName = itemName;
+        Quantity = quantity;
+        this.price = price;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        Quantity = quantity;
+    }
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "itemName='" + itemName + '\'' +
+                ", Quantity=" + Quantity +
+                ", price=" + price +
+                '}';
+    }
+}
